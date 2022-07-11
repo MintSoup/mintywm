176d1485408504ad8193f6c9eae4d20081eb690e
Areg <aregak2005@gmail.com>
2022-07-11 15:06:28 +0400

    Initial setup

---
+++ patch/include.h
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_ewmhtags.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "cfacts.h"
+#include "moveresize.h"
+#include "movestack.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
